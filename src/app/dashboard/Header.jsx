2ad57654e3a1ffdd74dc3380198b678b7d600b27
nabaraj basnet <nabarajbasnet2000@gmail.com>
2024-09-11 16:47:06 +0545

    Layout of admin dashboard created

---
+++ src/app/dashboard/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='fixed top-0 bg-green-400'>
+            <div className='bg-green-400'>
+                Header
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/app/dashboard/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sidebar = () => {
+    return (
+        <div className='fixed left-0'>
+            <div>
+                Sidebar
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
