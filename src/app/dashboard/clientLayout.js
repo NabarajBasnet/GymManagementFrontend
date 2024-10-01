da692448a3e7bb84efb662c4eef54908abeebc2f
Nabaraj Basnet <nabarajbasnet2000@gmail.com>
2024-10-01 16:18:10 -0700

    Responsive admin dashboard

--- src/app/dashboard/clientLayout.js
+++ src/app/dashboard/clientLayout.js
@@ -6,22 +6,27 @@ import RTKProvider from "@/state/ReduxProvider";
 import { useSelector } from "react-redux";
 
 export default function ClientLayout({ children }) {
-    
 
     const adminSidebar = useSelector(state => state.rtkreducer.adminSidebar);
-    console.log('Admin Sidebar In Layout: ',adminSidebar);
-
+    
     return (
         <RTKProvider>
-            <div>
-                <div>
-                    <div>
+            <div className="w-full">
+                <div className="w-full">
+                    <div className="w-full flex">
+                        {adminSidebar&&(
+                            <div className={`hidden md:flex`}>
                         <Sidebar />
+                        </div>
+                        )}
                     </div>
-                    <div>
-                        <Header />
-                        {children}
-                    </div>
+
+                    <div className={`w-full ${adminSidebar?'md:ml-64':''}`}>                    
+                            <Header />
+                            <div className="w-full">
+                                {children}
+                            </div>
+                        </div>
                 </div>
             </div>
         </RTKProvider>
