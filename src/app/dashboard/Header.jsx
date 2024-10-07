8361709a5836006bc6a282450957c9e034e8fcf6
nabaraj basnet <nabarajbasnet2000@gmail.com>
2024-10-07 22:22:06 +0545

    Blur added and transparency opened in header

--- src/app/dashboard/Header.jsx
+++ src/app/dashboard/Header.jsx
@@ -1,15 +1,13 @@
-"use client"
+"use client";
 
 import { RiAccountCircleFill } from "react-icons/ri";
-import React from 'react'
+import React from 'react';
 import { IoMenuSharp } from "react-icons/io5";
 import { ToggleAdminSidebar } from '@/state/slicer';
 import { useDispatch, useSelector } from 'react-redux';
 
 const Header = () => {
-
     const adminSidebar = useSelector(state => state.rtkreducer.adminSidebar);
-
     const dispatch = useDispatch();
 
     const handleDispatchSidebar = () => {
@@ -17,24 +15,22 @@ const Header = () => {
     };
 
     return (
-        <div className="w-full">
-            <div className={`fixed top-0 right-0 ${adminSidebar ? 'w-[calc(100%-240px)]' : 'w-full'}  flex justify-between py-4 items-center bg-green-500`}>
-                <div className='mx-4'>
-                    <IoMenuSharp
-                        className='text-3xl text-white cursor-pointer'
-                        onClick={handleDispatchSidebar}
-                    />
-                </div>
+        <div className={`fixed top-0 right-0 transition-all duration-300 ${adminSidebar ? 'w-[calc(100%-240px)]' : 'w-full'} flex justify-between py-4 items-center backdrop-blur-sm bg-white bg-opacity-70 z-50`}>
+            <div className='mx-4'>
+                <IoMenuSharp
+                    className='text-3xl text-gray-800 cursor-pointer'
+                    onClick={handleDispatchSidebar}
+                />
+            </div>
 
-                <div className='mx-4'>
-                    <RiAccountCircleFill
-                        className='text-3xl text-white cursor-pointer'
-                        onClick={handleDispatchSidebar}
-                    />
-                </div>
+            <div className='mx-4'>
+                <RiAccountCircleFill
+                    className='text-3xl text-gray-800 cursor-pointer'
+                    onClick={handleDispatchSidebar}
+                />
             </div>
         </div>
-    )
+    );
 }
 
 export default Header;
