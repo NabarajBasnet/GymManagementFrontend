a609ce48fa75d1a002271f50c78fc9ba4b40ab7e
nabaraj basnet <nabarajbasnet2000@gmail.com>
2024-10-13 23:00:12 +0545

    Basic change

--- src/app/dashboard/clientLayout.js
+++ src/app/dashboard/clientLayout.js
@@ -5,7 +5,6 @@ import Sidebar from "./Sidebar";
 import RTKProvider from "@/state/ReduxProvider";
 import { useSelector } from "react-redux";
 import DashboardFooter from "@/components/DashboardFooter/Footer";
-import { Toaster } from "@/components/ui/toaster";
 
 export default function ClientLayout({ children }) {
 
@@ -14,29 +13,27 @@ export default function ClientLayout({ children }) {
 
     return (
         <RTKProvider>
-            <Toaster>
-                <div className={`w-full flex`}>
-                    {adminSidebar && (
-                        <div className={`hidden md:flex`}>
-                            <Sidebar />
-                        </div>
-                    )}
-
-                    <div className={`w-full transition-all duration-500 ${sidebarMinimized ? 'md:ml-12' : 'md:ml-60'}`}>
-                        <div className="w-full mt-16">
-                            <Header />
-                        </div>
-                        <main className="w-full min-h-screen bg-gray-100 dark:bg-neutral-900 transition-all duration-500">
-                            <div className="w-full children-wrapper transition-transform duration-500 ease-in-out">
-                                {children}
-                            </div>
-                            <div className="w-full mt-3">
-                                <DashboardFooter />
-                            </div>
-                        </main>
+            <div className={`w-full flex`}>
+                {adminSidebar && (
+                    <div className={`hidden md:flex`}>
+                        <Sidebar />
                     </div>
+                )}
+
+                <div className={`w-full transition-all duration-500 ${sidebarMinimized ? 'md:ml-12' : 'md:ml-60'}`}>
+                    <div className="w-full mt-16">
+                        <Header />
+                    </div>
+                    <main className="w-full min-h-screen bg-gray-100 dark:bg-neutral-900 transition-all duration-500">
+                        <div className="w-full children-wrapper transition-transform duration-500 ease-in-out">
+                            {children}
+                        </div>
+                        <div className="w-full mt-3">
+                            <DashboardFooter />
+                        </div>
+                    </main>
                 </div>
-            </Toaster>
+            </div>
         </RTKProvider>
     );
 }
