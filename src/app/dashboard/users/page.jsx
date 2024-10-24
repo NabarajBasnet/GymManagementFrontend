34af73428e68e580b0b865384a60da560d57c668
nabaraj basnet <nabarajbasnet2000@gmail.com>
2024-10-24 08:30:15 +0545

    Updated users component

--- src/app/dashboard/users/page.jsx
+++ src/app/dashboard/users/page.jsx
@@ -1,3 +1,18 @@
+'use client';
+
+import * as React from "react";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import { IoIosMail } from "react-icons/io";
+import { TiUserDelete } from "react-icons/ti";
+import { FaUserEdit } from "react-icons/fa";
 import {
     Table,
     TableBody,
@@ -17,8 +32,57 @@ import {
     PaginationNext,
     PaginationPrevious,
 } from "@/components/ui/pagination";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+    Breadcrumb,
+    BreadcrumbEllipsis,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { useQuery } from '@tanstack/react-query';
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { IoMdClose } from "react-icons/io";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+
 
 const Users = () => {
+
+    const [usersMessage, setUsersMessage] = useState('');
+    const [userEditForm, setUserEditForm] = useState(false);
+
+    const getAllUsers = async () => {
+        try {
+            const response = await fetch(`http://localhost:5000/api/users`);
+            const responseBody = await response.json();
+            if (response.ok) {
+                setUsersMessage(responseBody.message);
+            }
+            return responseBody;
+        } catch (error) {
+            console.log('Error: ', error);
+        }
+    };
+
+    const { data, isLoading } = useQuery({
+        queryKey: (['users']),
+        queryFn: getAllUsers
+    });
+
+    const { users, message } = data || {};
+
+    console.log('Users: ', users);
+    console.log('Users: ', usersMessage);
+
 
     const invoices = [
         {
@@ -68,26 +132,176 @@ const Users = () => {
     return (
         <div className="w-full">
             <div className="w-full">
-                <div>
+                {
+                    userEditForm ? (
+                        <div className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50">
+                            <div className="flex justify-center bg-transparent p-4 rounded-sm shadow-lg w-full">
+                                <div className="bg-white p-4 rounded-sm shadow-lg md:w-8/12 w-full">
+                                    <div className="w-full flex justify-between items-center">
+                                        <h2 className="text-lg font-bold mb-4">Edit User Details</h2>
+                                        <IoMdClose
+                                            className="text-lg cursor-pointer"
+                                            onClick={() => setUserEditForm(false)}
+                                        />
+                                    </div>
+
+                                    <form className="w-full">
+                                        <div className="w-full flex items-center space-x-4">
+                                            <div className="w-full">
+                                                <Label>First Name</Label>
+                                                <Input
+                                                    className='rounded-none'
+                                                    placeholder='First Name'
+                                                />
+                                            </div>
+
+                                            <div className="w-full">
+                                                <Label>First Name</Label>
+                                                <Input
+                                                    className='rounded-none'
+                                                    placeholder='First Name'
+                                                />
+                                            </div>
+                                        </div>
+
+                                        <div>
+                                            <Label>User Role</Label>
+                                            <Select>
+                                                <SelectTrigger className="rounded-none">
+                                                    <SelectValue placeholder="Assign role" />
+                                                </SelectTrigger>
+                                                <SelectContent>
+                                                    <SelectGroup>
+                                                        <SelectLabel>Select</SelectLabel>
+                                                        <SelectItem value="Super Admin">Super Admin</SelectItem>
+                                                        <SelectItem value="Admin">Admin</SelectItem>
+                                                        <SelectItem value="Moderator">Moderator</SelectItem>
+                                                        <SelectItem value="User">User</SelectItem>
+                                                    </SelectGroup>
+                                                </SelectContent>
+                                            </Select>
+                                        </div>
+
+                                        <div className="w-full">
+                                            <Label>Email</Label>
+                                            <Input
+                                                className='rounded-none'
+                                                placeholder='Email'
+                                            />
+                                        </div>
+
+                                        <div className="w-full">
+                                            <Label>Phone Number</Label>
+                                            <Input
+                                                className='rounded-none'
+                                                placeholder='Phone Number'
+                                            />
+                                        </div>
+
+                                        <div className="w-full">
+                                            <Label>Address</Label>
+                                            <Input
+                                                className='rounded-none'
+                                                placeholder='Address'
+                                            />
+                                        </div>
+
+                                    </form>
+
+                                    <div className="w-full flex mt-4 space-x-4 justify-center">
+                                        <Button
+                                            onClick={() => setUserEditForm(false)}
+                                            className="rounded-none text-white font-bold py-2 px-4"
+                                        >
+                                            Submit
+                                        </Button>
+                                        <Button
+                                            variant='destructive'
+                                            onClick={() => setUserEditForm(false)}
+                                            className="rounded-none text-white font-bold py-2 px-4"
+                                        >
+                                            Cancel
+                                        </Button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ) : (
+                        <></>
+                    )
+                }
+                <div className='w-full p-6'>
+                    <Breadcrumb>
+                        <BreadcrumbList>
+                            <BreadcrumbItem>
+                                <BreadcrumbLink href="/">Home</BreadcrumbLink>
+                            </BreadcrumbItem>
+                            <BreadcrumbSeparator />
+                            <BreadcrumbItem>
+                                <DropdownMenu>
+                                    <DropdownMenuTrigger className="flex items-center gap-1">
+                                        <BreadcrumbEllipsis className="h-4 w-4" />
+                                        <span className="sr-only">Toggle menu</span>
+                                    </DropdownMenuTrigger>
+                                    <DropdownMenuContent align="start">
+                                        <DropdownMenuItem>Documentation</DropdownMenuItem>
+                                        <DropdownMenuItem>Themes</DropdownMenuItem>
+                                        <DropdownMenuItem>GitHub</DropdownMenuItem>
+                                    </DropdownMenuContent>
+                                </DropdownMenu>
+                            </BreadcrumbItem>
+                            <BreadcrumbSeparator />
+                            <BreadcrumbItem>
+                                <BreadcrumbLink href="/docs/components">Components</BreadcrumbLink>
+                            </BreadcrumbItem>
+                            <BreadcrumbSeparator />
+                            <BreadcrumbItem>
+                                <BreadcrumbPage>Users</BreadcrumbPage>
+                            </BreadcrumbItem>
+                        </BreadcrumbList>
+                    </Breadcrumb>
+                    <h1 className="text-xl font-bold mt-3">Users</h1>
+                </div>
+
+                <div className="bg-white">
                     <Table>
-                        <TableCaption>A list of your recent invoices.</TableCaption>
+                        <TableCaption>A list of users.</TableCaption>
                         <TableHeader>
                             <TableRow>
-                                <TableHead className="w-[100px]">Invoice</TableHead>
-                                <TableHead>Status</TableHead>
-                                <TableHead>Method</TableHead>
-                                <TableHead className="text-right">Amount</TableHead>
+                                <TableHead>Full Name</TableHead>
+                                <TableHead>Role</TableHead>
+                                <TableHead>Email</TableHead>
+                                <TableHead>Phone Number</TableHead>
+                                <TableHead>Address</TableHead>
+                                <TableHead>Actions</TableHead>
                             </TableRow>
                         </TableHeader>
                         <TableBody>
-                            {invoices.map((invoice) => (
-                                <TableRow key={invoice.invoice}>
-                                    <TableCell className="font-medium">{invoice.invoice}</TableCell>
-                                    <TableCell>{invoice.paymentStatus}</TableCell>
-                                    <TableCell>{invoice.paymentMethod}</TableCell>
-                                    <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+                            {Array.isArray(users) && users.length > 0 ? (
+                                users.map((user) => (
+                                    <TableRow key={user._id}>
+                                        <TableCell className="font-medium">{user.firstName + user.lastName}</TableCell>
+                                        <TableCell>{'Role'}</TableCell>
+                                        <TableCell>{user.email}</TableCell>
+                                        <TableCell>{user.phoneNumber}</TableCell>
+                                        <TableCell>{user.address}</TableCell>
+                                        <TableCell>
+                                            <div className="flex justify-center items-center">
+                                                <FaUserEdit
+                                                    onClick={() => setUserEditForm(true)}
+                                                    className="text-lg cursor-pointer"
+                                                />
+                                                <TiUserDelete className="text-lg mx-2 cursor-pointer" />
+                                                <IoIosMail className="text-lg cursor-pointer" />
+                                            </div>
+                                        </TableCell>
+                                    </TableRow>
+                                ))
+                            ) : (
+                                <TableRow>
+                                    <TableCell colSpan='12' className="text-center">Users not found.</TableCell>
                                 </TableRow>
-                            ))}
+                            )}
                         </TableBody>
                         <TableFooter>
                             <TableRow>
