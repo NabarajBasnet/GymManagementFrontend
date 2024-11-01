e1ae650cc2e2a19b214486564f72ade29a0fe7b5
nabaraj basnet <nabarajbasnet2000@gmail.com>
2024-11-02 02:19:03 +0545

    Middleware updated

--- src/middleware.js
+++ src/middleware.js
@@ -5,13 +5,17 @@ export function middleware(req) {
     const url = req.nextUrl.clone();
     const token = req.cookies.get('loginToken');
 
+    console.log("Retrieved Token:", token);
+
     if (!token) {
+        console.log("No token found. Redirecting to /login.");
         url.pathname = '/login';
         return NextResponse.redirect(url);
     }
 
     try {
         jwt.verify(token, process.env.TOKEN_SECRET);
+        console.log("Token verified successfully.");
     } catch (error) {
         console.log("Invalid Token:", error);
         url.pathname = '/login';
