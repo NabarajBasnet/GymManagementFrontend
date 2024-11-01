9ad3743ebf991111400c8f84f9ab82b2cb24a8f0
nabaraj basnet <nabarajbasnet2000@gmail.com>
2024-11-02 02:22:43 +0545

    Middleware disabled

--- src/middleware.js
+++ src/middleware.js
@@ -1,30 +1,30 @@
-import { NextResponse } from 'next/server';
-import jwt from 'jsonwebtoken';
+// import { NextResponse } from 'next/server';
+// import jwt from 'jsonwebtoken';
 
-export function middleware(req) {
-    const url = req.nextUrl.clone();
-    const token = req.cookies.get('loginToken');
+// export function middleware(req) {
+//     const url = req.nextUrl.clone();
+//     const token = req.cookies.get('loginToken');
 
-    console.log("Retrieved Token:", token);
+//     console.log("Retrieved Token:", token);
 
-    if (!token) {
-        console.log("No token found. Redirecting to /login.");
-        url.pathname = '/login';
-        return NextResponse.redirect(url);
-    }
+//     if (!token) {
+//         console.log("No token found. Redirecting to /login.");
+//         url.pathname = '/login';
+//         return NextResponse.redirect(url);
+//     }
 
-    try {
-        jwt.verify(token, process.env.TOKEN_SECRET);
-        console.log("Token verified successfully.");
-    } catch (error) {
-        console.log("Invalid Token:", error);
-        url.pathname = '/login';
-        return NextResponse.redirect(url);
-    }
+//     try {
+//         jwt.verify(token, process.env.TOKEN_SECRET);
+//         console.log("Token verified successfully.");
+//     } catch (error) {
+//         console.log("Invalid Token:", error);
+//         url.pathname = '/login';
+//         return NextResponse.redirect(url);
+//     }
 
-    return NextResponse.next();
-}
+//     return NextResponse.next();
+// }
 
-export const config = {
-    matcher: ['/dashboard/:path*'],
-};
+// export const config = {
+//     matcher: ['/dashboard/:path*'],
+// };
