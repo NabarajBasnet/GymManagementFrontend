2697a0242e8b9b89d56accc494c4d7e437aa6742
nabaraj basnet <nabarajbasnet2000@gmail.com>
2024-11-02 21:25:19 +0545

    updated middleware

--- src/middleware.js
+++ src/middleware.js
@@ -5,12 +5,11 @@ export const middleware = async (request) => {
     const token = request.cookies.get('loginToken')?.value || '';
 
     try {
-        if (token) {
-            return NextResponse.redirect(new URL('/dashboard'))
-        } else {
+        if (!token && path.startsWith('/dashboard')) {
             return NextResponse.redirect(new URL('/login', request.url));
+        } else if (token && (path === '/login' || path === '/signup')) {
+            return NextResponse.redirect(new URL('/dashboard', request.url));
         }
-
     } catch (error) {
         console.error('Error verifying token:', error);
     }
@@ -19,5 +18,5 @@ export const middleware = async (request) => {
 }
 
 export const config = {
-    matcher: ['/login', '/signup', '/dashboard/:path*']
+    matcher: ['/login', '/signup', '/dashboard/:path*'],
 };
