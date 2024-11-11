aa6e39a453a9f3426c3addbee5f5dabf9d484fee
Nabaraj Basnet <112648953+NabarajBasnet@users.noreply.github.com>
2024-11-11 16:37:11 +0545

    Update page.jsx

--- src/app/dashboard/attendance/attendancehistory/page.jsx
+++ src/app/dashboard/attendance/attendancehistory/page.jsx
@@ -285,8 +285,8 @@ const AttendanceHistory = () => {
                             </TableBody>
                             <TableFooter>
                                 <TableRow>
-                                    <TableCell colSpan={3}>Total</TableCell>
-                                    <TableCell className="text-right">$2,500.00</TableCell>
+                                    <TableCell colSpan={3}>Total checked in time</TableCell>
+                                    <TableCell>{PermanentMemberAttendance.length}</TableCell>
                                 </TableRow>
                             </TableFooter>
                         </Table>
