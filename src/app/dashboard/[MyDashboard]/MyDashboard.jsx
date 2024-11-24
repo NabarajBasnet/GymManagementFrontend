f85321be1f9cbad559c83de33e2baf849e100d68
nabaraj basnet <nabarajbasnet2000@gmail.com>
2024-11-24 16:52:49 +0545

    Creating staff dashboard

---
+++ src/app/dashboard/[MyDashboard]/MyDashboard.jsx
@@ -0,0 +1,15 @@
+const MyDashboard = () => {
+    try {
+
+    } catch (error) {
+
+    }
+
+    return (
+        <div>
+            <h1>My Dashboard</h1>
+        </div>
+    )
+}
+
+export default MyDashboard;
---
+++ src/app/dashboard/[MyDashboard]/page.jsx
@@ -0,0 +1,13 @@
+import MyDashboard from "./MyDashboard";
+
+const Me = () => {
+    try {
+
+    } catch (error) {
+        console.log("Error: ", error)
+    }
+
+    return <MyDashboard />
+}
+
+export default Me;
