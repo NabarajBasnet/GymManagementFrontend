53caefb7bd494c3c8205fd0976fb74f9b428aec3
nabaraj basnet <nabarajbasnet2000@gmail.com>
2024-11-26 14:14:59 +0545

    Conditionally render staff details in profile page

--- src/app/MyProfile/page.jsx
+++ src/app/MyProfile/page.jsx
@@ -8,7 +8,7 @@ import {
     PaginationLink,
     PaginationNext,
     PaginationPrevious,
-} from "@/components/ui/pagination"
+} from "@/components/ui/pagination";
 import {
     Table,
     TableBody,
@@ -18,9 +18,9 @@ import {
     TableHead,
     TableHeader,
     TableRow,
-} from "@/components/ui/table"
-import { useQuery } from "@tanstack/react-query"
-import { useState, useEffect } from "react"
+} from "@/components/ui/table";
+import { useQuery } from "@tanstack/react-query";
+import { useState, useEffect } from "react";
 
 const MyProfile = () => {
 
@@ -37,7 +37,7 @@ const MyProfile = () => {
 
     const fetchedLoggedStaffDetails = async () => {
         try {
-            const response = await fetch(`http://88.198.112.156:3000/api/loggedin-staff`);
+            const response = await fetch(`http://localhost:3000/api/loggedin-staff`);
             const responseBody = await response.json();
             if (response.ok) {
                 setStaffDetails(responseBody.loggedInStaff)
@@ -55,7 +55,7 @@ const MyProfile = () => {
 
     const fetchStaffQr = async () => {
         try {
-            const response = await fetch(`http://88.198.112.156:3000/api/staffqr/${staffDetails._id}`);
+            const response = await fetch(`http://localhost:3000/api/staffqr/${staffDetails._id}`);
             const responseBody = await response.json();
             return responseBody;
         } catch (error) {
@@ -117,137 +117,142 @@ const MyProfile = () => {
     return (
         <div className="w-full">
             <h1 className="text-center text-4xl font-bold my-4">My Profile</h1>
-            {isLoading || !data ? (
+            {isLoading ? (
                 <h1 className="text-center font-semibold">Loading...</h1>
             ) : (
-                <div className="w-full flex items-center justify-center p-1">
-                    <img src={data.qrCode} alt="QR Code" />
-                </div>
+                <>
+                    <div className="w-full flex items-center justify-center p-1">
+                        <img src={data?.qrCode} alt="QR Code" />
+                    </div>
+                    <div className="w-full">
+                        <h1 className="text-center my-4">
+                            Current Time: {currentTime?.toISOString().split("T")[0]},{" "}
+                            {currentTime?.toLocaleTimeString("en-US", {
+                                hour: "2-digit",
+                                minute: "2-digit",
+                                second: "2-digit",
+                                hour12: true,
+                            })}
+                        </h1>
+                    </div>
+                    <div className="w-full flex justify-center bg-black p-6 text-white">
+                        <div className="w-full md:w-9/12">
+                            <h1 className="text-3xl text-yellow-400 font-bold">
+                                {staffDetails?.fullName} - {staffDetails?.role}
+                            </h1>
+                            <p>Shift: {staffDetails?.shift}</p>
+                        </div>
+                    </div>
+                    <div className="w-full max-w-md mx-auto bg-white shadow-xl my-4 rounded-lg p-6">
+                        <div className="space-y-4">
+                            <div className="flex items-center justify-between">
+                                <span className="font-medium text-gray-700">Phone No:</span>
+                                <span className="text-gray-600">{staffDetails?.contactNo}</span>
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <span className="font-medium text-gray-700">Email:</span>
+                                <span className="text-gray-600">{staffDetails?.email}</span>
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <span className="font-medium text-gray-700">Date of Birth:</span>
+                                {
+                                    staffDetails ? (
+                                        <span className="text-gray-600">
+                                            {new Date(staffDetails?.dob).toISOString().split("T")[0]}
+                                        </span>
+                                    ) : (
+                                        <p></p>
+                                    )
+                                }
+
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <span className="font-medium text-gray-700">Address:</span>
+                                <span className="text-gray-600">{staffDetails?.address}</span>
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <span className="font-medium text-gray-700">Role:</span>
+                                <span className="text-gray-600">{staffDetails?.role}</span>
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <span className="font-medium text-gray-700">Joined Date:</span>
+                                {
+                                    staffDetails ? (
+                                        <span className="text-gray-600">
+                                            {new Date(staffDetails?.joinedDate).toISOString().split("T")[0]}
+                                        </span>
+                                    ) : (
+                                        <p></p>
+                                    )
+                                }
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <span className="font-medium text-gray-700">Gender:</span>
+                                <span className="text-gray-600">{staffDetails?.gender}</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <h1>My Attendance History</h1>
+                        <Table>
+                            <TableCaption>A list of your recent invoices.</TableCaption>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead className="w-[100px]">Staff Id</TableHead>
+                                    <TableHead>Name</TableHead>
+                                    <TableHead>Email</TableHead>
+                                    <TableHead>Check In</TableHead>
+                                    <TableHead>Check Out</TableHead>
+                                    <TableHead>Remark</TableHead>
+                                    <TableHead>Late Flag</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                {invoices.map((invoice) => (
+                                    <TableRow key={invoice.invoice}>
+                                        <TableCell className="font-medium">{invoice.invoice}</TableCell>
+                                        <TableCell>{invoice.paymentStatus}</TableCell>
+                                        <TableCell>{invoice.paymentMethod}</TableCell>
+                                        <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                            <TableFooter>
+                                <TableRow>
+                                    <TableCell colSpan={3}>Total</TableCell>
+                                    <TableCell className="text-right">$2,500.00</TableCell>
+                                </TableRow>
+                            </TableFooter>
+                        </Table>
+                        <div className="my-4">
+                            <Pagination>
+                                <PaginationContent>
+                                    <PaginationItem>
+                                        <PaginationPrevious href="#" />
+                                    </PaginationItem>
+                                    <PaginationItem>
+                                        <PaginationLink href="#">1</PaginationLink>
+                                    </PaginationItem>
+                                    <PaginationItem>
+                                        <PaginationLink href="#" isActive>
+                                            2
+                                        </PaginationLink>
+                                    </PaginationItem>
+                                    <PaginationItem>
+                                        <PaginationLink href="#">3</PaginationLink>
+                                    </PaginationItem>
+                                    <PaginationItem>
+                                        <PaginationEllipsis />
+                                    </PaginationItem>
+                                    <PaginationItem>
+                                        <PaginationNext href="#" />
+                                    </PaginationItem>
+                                </PaginationContent>
+                            </Pagination>
+                        </div>
+                    </div>
+                </>
             )}
-            <div className="w-full">
-                {currentTime ? (
-                    <h1 className="text-center my-4">
-                        Current Time: {currentTime.toISOString().split("T")[0]},{" "}
-                        {currentTime.toLocaleTimeString("en-US", {
-                            hour: "2-digit",
-                            minute: "2-digit",
-                            second: "2-digit",
-                            hour12: true,
-                        })}
-                    </h1>
-                ) : (
-                    <h1 className="text-center font-semibold my-4">Loading current time...</h1>
-                )}
-            </div>
-            <div className="w-full flex justify-center bg-black p-6 text-white">
-                {isLoading ? (
-                    <h1>Loading...</h1>
-                ) : (
-                    <div className="w-full md:w-9/12">
-                        <h1 className="text-3xl text-yellow-400 font-bold">{staffDetails.fullName} - {staffDetails.role}</h1>
-                        <p>Shift: {staffDetails.shift}</p>
-                    </div>
-                )}
-
-            </div>
-            <div className="w-full max-w-md mx-auto bg-white shadow-xl my-4 rounded-lg p-6">
-                {isLoading ? (
-                    <h1 className="text-center items-center font-semibold">Loading...</h1>
-                ) : (
-                    <div className="space-y-4">
-                        <div className="flex items-center justify-between">
-                            <span className="font-medium text-gray-700">Phone No:</span>
-                            <span className="text-gray-600">{staffDetails.contactNo}</span>
-                        </div>
-                        <div className="flex items-center justify-between">
-                            <span className="font-medium text-gray-700">Email:</span>
-                            <span className="text-gray-600">{staffDetails.email}</span>
-                        </div>
-                        <div className="flex items-center justify-between">
-                            <span className="font-medium text-gray-700">Date of Birth:</span>
-                            <span className="text-gray-600">{new Date(staffDetails.dob).toISOString().split("T")[0]}</span>
-                        </div>
-                        <div className="flex items-center justify-between">
-                            <span className="font-medium text-gray-700">Address:</span>
-                            <span className="text-gray-600">{staffDetails.address}</span>
-                        </div>
-                        <div className="flex items-center justify-between">
-                            <span className="font-medium text-gray-700">Role:</span>
-                            <span className="text-gray-600">{staffDetails.role}</span>
-                        </div>
-                        <div className="flex items-center justify-between">
-                            <span className="font-medium text-gray-700">Joined Date:</span>
-                            <span className="text-gray-600">{new Date(staffDetails.joinedDate).toISOString().split("T")[0]}</span>
-                        </div>
-                        <div className="flex items-center justify-between">
-                            <span className="font-medium text-gray-700">Gender:</span>
-                            <span className="text-gray-600">{staffDetails.gender}</span>
-                        </div>
-                    </div>
-                )}
-            </div>
-
-            <div>
-                <h1>My Attendance History</h1>
-
-                <Table>
-                    <TableCaption>A list of your recent invoices.</TableCaption>
-                    <TableHeader>
-                        <TableRow>
-                            <TableHead className="w-[100px]">Staff Id</TableHead>
-                            <TableHead>Name</TableHead>
-                            <TableHead>Email</TableHead>
-                            <TableHead>Check In</TableHead>
-                            <TableHead>Check Out</TableHead>
-                            <TableHead>Remark</TableHead>
-                            <TableHead>Late Flag</TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        {invoices.map((invoice) => (
-                            <TableRow key={invoice.invoice}>
-                                <TableCell className="font-medium">{invoice.invoice}</TableCell>
-                                <TableCell>{invoice.paymentStatus}</TableCell>
-                                <TableCell>{invoice.paymentMethod}</TableCell>
-                                <TableCell className="text-right">{invoice.totalAmount}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                    <TableFooter>
-                        <TableRow>
-                            <TableCell colSpan={3}>Total</TableCell>
-                            <TableCell className="text-right">$2,500.00</TableCell>
-                        </TableRow>
-                    </TableFooter>
-                </Table>
-
-                <div className="my-4">
-                    <Pagination>
-                        <PaginationContent>
-                            <PaginationItem>
-                                <PaginationPrevious href="#" />
-                            </PaginationItem>
-                            <PaginationItem>
-                                <PaginationLink href="#">1</PaginationLink>
-                            </PaginationItem>
-                            <PaginationItem>
-                                <PaginationLink href="#" isActive>
-                                    2
-                                </PaginationLink>
-                            </PaginationItem>
-                            <PaginationItem>
-                                <PaginationLink href="#">3</PaginationLink>
-                            </PaginationItem>
-                            <PaginationItem>
-                                <PaginationEllipsis />
-                            </PaginationItem>
-                            <PaginationItem>
-                                <PaginationNext href="#" />
-                            </PaginationItem>
-                        </PaginationContent>
-                    </Pagination>
-                </div>
-            </div>
         </div>
     )
 }
