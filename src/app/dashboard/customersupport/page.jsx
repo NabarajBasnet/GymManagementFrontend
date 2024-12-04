b7bd45d6f77e37251f40a63bb2a6f2d081df97a7
nabaraj basnet <nabarajbasnet2000@gmail.com>
2024-12-04 16:17:09 +0545

    Learning custom pagination

--- src/app/dashboard/customersupport/page.jsx
+++ src/app/dashboard/customersupport/page.jsx
@@ -1,12 +1,88 @@
+'use client';
+
+import { BsThreeDots } from "react-icons/bs";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+import Loader from "@/components/Loader/Loader";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 const CustomerSupport = () => {
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const limit = 12;
+    console.log("Current Page: ", currentPage);
+
+    const getAllMembers = async ({ queryKey }) => {
+        const [, page] = queryKey
+
+        try {
+            const response = await fetch(`http://88.198.112.156:3000/api/members?page=${page}&limit=${limit}`);
+            const resBody = await response.json();
+            console.log("Response Body: ", resBody);
+            return resBody;
+        } catch (error) {
+            console.log('Error: ', error);
+        }
+    };
+
+    const { data, isLoading } = useQuery({
+        queryKey: (['members', currentPage]),
+        queryFn: getAllMembers
+    });
+
+    const { totalPages, totalMembers, members } = data || {};
+
+    console.log("Data: ", data);
+
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
+    };
+
     return (
-        <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-gray-200 text-center px-6">
-            <h1 className="text-4xl font-bold mb-4 animate-pulse">🚧 Page Under Development 🚧</h1>
-            <p className="text-lg max-w-md mb-8">
-                We’re working hard to bring this page to life. Stay tuned for updates!
-            </p>
-            <div className="w-12 h-12 border-4 border-t-transparent border-gray-300 rounded-full animate-spin"></div>
+        <div className="flex flex-col items-center justify-center h-screen text-center px-6">
+            {
+                isLoading ? (
+                    <Loader />
+                ) : (
+                    <div>
+                        {data.members.map((member) => (
+                            <p key={member._id}>{member.fullName}</p>
+                        ))}
+                    </div>
+                )
+            }
+            <div className="mt-10">
+                <div className="flex items-center space-x-2">
+                    <FaAngleLeft className="cursor-pointer text-xl" />
+                    <Button
+                        className={`bg-transparent hover:bg-gray-100 text-black`}
+                        disabled={currentPage === 1}
+                        onClick={() => handlePageChange(currentPage - 1)}>Previous</Button>
+                    {
+                        [...Array(totalPages)].map((_, i) => {
+                            return (
+                                <>
+                                    <p
+                                        className={`px-4 py-1 cursor-pointer rounded-lg ${currentPage === i + 1 ? 'border border-blue-600' : ''}`}
+                                        onClick={() => handlePageChange(i + 1)}
+                                    >{i + 1}</p>
+                                </>
+                            )
+                        })
+                    }
+                    <BsThreeDots />
+                    <Button
+                        className={`bg-transparent hover:bg-gray-100 text-black`}
+                        disabled={currentPage === totalPages}
+                        onClick={() => handlePageChange(currentPage + 1)}>Next</Button>
+                    <FaAngleRight className="cursor-pointer text-xl" />
+                </div>
+            </div>
+
+            <div className="mt-10">
+                <p>Current Page: {currentPage / 2}</p>
+            </div>
         </div>
     );
 }
