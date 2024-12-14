459fbbbd837ea9e9c2c336d58044b02fd0cf044d
nabaraj basnet <nabarajbasnet2000@gmail.com>
2024-12-15 00:15:30 +0545

    Render payment details of selected members

--- src/app/dashboard/paymentdetails/page.jsx
+++ src/app/dashboard/paymentdetails/page.jsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { Button } from "@/components/ui/button";
-import { IoSearch } from "react-icons/io5";
+import Pagination from "@/components/ui/CustomPagination";
+import { Label } from "@/components/ui/label";
 import {
     Breadcrumb,
     BreadcrumbEllipsis,
@@ -11,15 +11,6 @@ import {
     BreadcrumbPage,
     BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
-import {
-    Pagination,
-    PaginationContent,
-    PaginationEllipsis,
-    PaginationItem,
-    PaginationLink,
-    PaginationNext,
-    PaginationPrevious,
-} from "@/components/ui/pagination";
 import {
     Table,
     TableBody,
@@ -37,18 +28,86 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
-
+import { useQuery } from "@tanstack/react-query";
+import { useRef, useState, useEffect } from "react";
+import { useForm, Controller } from "react-hook-form";
+import Loader from "@/components/Loader/Loader";
+import { usePagination } from "@/hooks/Pagination";
 
 const PaymentDetails = () => {
 
-    const invoices = [
-        {
-            invoice: "INV001",
-            paymentStatus: "Paid",
-            totalAmount: "$250.00",
-            paymentMethod: "Credit Card",
+    const [searchQuery, setSearchQuery] = useState('');
+    const [memberName, setMemberName] = useState('');
+    const [memberId, setMemberId] = useState('');
+    const [renderDropdown, setRenderDropdown] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1)
+    const limit = 10;
+
+    const { control, formState: { errors, isSubmitting } } = useForm();
+
+    const getAllMembers = async () => {
+        try {
+            const response = await fetch(`http://localhost:3000/api/members`);
+            const responseBody = await response.json();
+            return responseBody;
+        } catch (error) {
+            console.log("Error: ", error);
+        };
+    };
+
+    const { data, isLoading } = useQuery({
+        queryKey: ['members'],
+        queryFn: getAllMembers
+    });
+
+    const { members } = data || {};
+
+    const getPaymentDetails = async ({ queryKey }) => {
+        const [, page, memberId] = queryKey
+        try {
+            const response = await fetch(`http://localhost:3000/api/paymentdetails/${memberId}?page=${page}&limit=${limit}`);
+            const responseBody = await response.json();
+            return responseBody;
+        } catch (error) {
+            console.log("Error: ", error);
+        };
+    };
+
+    const { data: paymentDetails, isLoading: isPaymentDetailLoading } = useQuery({
+        queryKey: ['paymentDetails', currentPage, memberId],
+        queryFn: getPaymentDetails,
+        enabled: !!memberId
+    });
+
+    const { paginatedPaymentDetails, totalPages, totalPaymentDetails } = paymentDetails || {}
+    const { range, setPage, active } = usePagination({
+        total: totalPages ? totalPages : 1,
+        siblings: 1,
+        boundaries: 1,
+        page: currentPage,
+        onChange: (page) => {
+            setCurrentPage(page);
         },
-    ];
+    });
+
+    const searchRef = useRef(null)
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (searchRef.current && !searchRef.current.contains(event.target)) {
+                setRenderDropdown(false);
+            }
+        };
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [searchRef]);
+
+    const handleSearchFocus = () => {
+        setRenderDropdown(true);
+    };
+
 
     return (
         <div className="w-full">
@@ -86,71 +145,117 @@ const PaymentDetails = () => {
             </div>
 
             <div className="w-full bg-white p-4">
-                <div className="w-full md:flex items-center">
-                    <div className="w-full flex items-center border px-4 my-2">
-                        <IoSearch />
-                        <Input
-                            className='rounded-none border-none'
-                            placeholder='Search member...'
-                        />
+                <form className="w-full flex flex-col md:flex-row items-start md:items-center space-y-4 md:space-y-0 md:space-x-4">
+                    <div className="w-full">
+                        <Label className="block text-sm font-medium text-gray-700">Member Name</Label>
+                        <div ref={searchRef} className="relative md:flex items-center m-2">
+                            <Controller
+                                name="memberName"
+                                control={control}
+                                render={({ field }) => (
+                                    <Input
+                                        {...field}
+                                        autoComplete="off"
+                                        value={searchQuery}
+                                        onChange={(e) => {
+                                            setSearchQuery(e.target.value);
+                                            field.onChange(e);
+                                        }}
+                                        onFocus={handleSearchFocus}
+                                        className="w-full rounded-lg border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-sm px-4 md:py-0 py-2"
+                                        placeholder="Search members..."
+                                    />
+                                )}
+                            />
+                            {errors.memberName && (
+                                <p className="mt-1 text-sm font-semibold text-red-600">
+                                    {errors.memberName.message}
+                                </p>
+                            )}
+
+                            {/* Dropdown Section */}
+                            {renderDropdown && (
+                                <div className="absolute w-full mt-1 bg-white border border-gray-300 rounded-lg shadow-md max-h-80 overflow-y-auto z-20">
+                                    {members
+                                        ?.filter((member) => {
+                                            const matchByName = member.fullName
+                                                .toLowerCase()
+                                                .includes(searchQuery.toLowerCase());
+                                            return matchByName;
+                                        })
+                                        .map((member) => (
+                                            <p
+                                                onClick={() => {
+                                                    setMemberName(member.fullName);
+                                                    setSearchQuery(member.fullName);
+                                                    setMemberId(member._id);
+                                                    setRenderDropdown(false);
+                                                }}
+                                                className="px-4 py-2 text-sm text-gray-700 hover:bg-blue-100 cursor-pointer"
+                                                key={member._id}
+                                                value={member._id}
+                                            >
+                                                {member.fullName}
+                                            </p>
+                                        ))}
+                                </div>
+                            )}
+                        </div>
                     </div>
-                    <Button className='md:mx-4 mx-0 my-4 md:my-0'>Submit</Button>
-                </div>
+                </form>
 
                 <div>
-                    <Table>
-                        <TableHeader>
-                            <TableRow className='bg-gray-200 text-black'>
-                                <TableHead>Invoice</TableHead>
-                                <TableHead>Status</TableHead>
-                                <TableHead>Method</TableHead>
-                                <TableHead className="text-right">Amount</TableHead>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                            {invoices.map((invoice) => (
-                                <TableRow key={invoice.invoice}>
-                                    <TableCell className="font-medium">{invoice.invoice}</TableCell>
-                                    <TableCell>{invoice.paymentStatus}</TableCell>
-                                    <TableCell>{invoice.paymentMethod}</TableCell>
-                                    <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+                    {isLoading ? (
+                        <Loader />
+                    ) : (
+                        <Table>
+                            <TableHeader>
+                                <TableRow className='bg-gray-200 text-black'>
+                                    <TableHead>Invoice</TableHead>
+                                    <TableHead>Status</TableHead>
+                                    <TableHead>Method</TableHead>
+                                    <TableHead className="text-right">Amount</TableHead>
                                 </TableRow>
-                            ))}
-                        </TableBody>
-                        <TableFooter>
-                            <TableRow>
-                                <TableCell colSpan={3}>Total Payments</TableCell>
-                                <TableCell className="text-right">5</TableCell>
-                            </TableRow>
-                        </TableFooter>
-                    </Table>
+                            </TableHeader>
+                            <TableBody>
+                                {Array.isArray(paginatedPaymentDetails) && paginatedPaymentDetails.length >= 1 ? (
+                                    <>
+                                        {paginatedPaymentDetails.map((detail) => (
+                                            <TableRow key={detail._id}>
+                                                <TableCell className="font-medium">{paginatedPaymentDetails ? detail.member.fullName : ''}</TableCell>
+                                                <TableCell>{detail.paidAmmount}</TableCell>
+                                                <TableCell>{detail.actionTaker}</TableCell>
+                                                <TableCell className="text-right">{detail.discountAmmount}</TableCell>
+                                            </TableRow>
+                                        ))}
+                                    </>
+                                ) : (
+                                    <TableRow>
+                                        <TableCell className="text-center">No payment details recorded.</TableCell>
+                                    </TableRow>
+                                )}
+
+
+                            </TableBody>
+                            <TableFooter>
+                                <TableRow>
+                                    <TableCell colSpan={3}>Total Payments</TableCell>
+                                    <TableCell className="text-right">5</TableCell>
+                                </TableRow>
+                            </TableFooter>
+                        </Table>
+                    )}
                 </div>
 
                 <div className="py-3">
-                    <Pagination>
-                        <PaginationContent>
-                            <PaginationItem>
-                                <PaginationPrevious href="#" />
-                            </PaginationItem>
-                            <PaginationItem>
-                                <PaginationLink href="#">1</PaginationLink>
-                            </PaginationItem>
-                            <PaginationItem>
-                                <PaginationLink href="#" isActive>
-                                    2
-                                </PaginationLink>
-                            </PaginationItem>
-                            <PaginationItem>
-                                <PaginationLink href="#">3</PaginationLink>
-                            </PaginationItem>
-                            <PaginationItem>
-                                <PaginationEllipsis />
-                            </PaginationItem>
-                            <PaginationItem>
-                                <PaginationNext href="#" />
-                            </PaginationItem>
-                        </PaginationContent>
-                    </Pagination>
+                    <Pagination
+                        total={totalPages || 1}
+                        page={currentPage || 1}
+                        onChange={setCurrentPage}
+                        withEdges={true}
+                        siblings={1}
+                        boundaries={1}
+                    />
                 </div>
             </div>
         </div>
