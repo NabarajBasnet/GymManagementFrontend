908eeb9dfc54789469df693b5ce4aa236701eb90
nabaraj basnet <nabarajbasnet2000@gmail.com>
2025-02-12 18:28:11 +0545

    Getting data from previous date

--- src/app/dashboard/logs/page.jsx
+++ src/app/dashboard/logs/page.jsx
@@ -1,9 +1,34 @@
+'use client'
+
+import { Button } from '@/components/ui/button';
 import React from 'react';
 
 const Logs = () => {
+    const fetchData = async () => {
+        try {
+            const response = await fetch('http://88.198.112.156:3000/api/averageactivemembers');
+            if (!response.ok) {
+                const errorText = await response.text();
+                throw new Error(`Network response was not ok. Status: ${response.status}, Message: ${errorText}`);
+            }
+            let data;
+            try {
+                data = await response.json();
+            } catch (jsonError) {
+                throw new Error('Failed to parse JSON response');
+            }
+            console.log(data);
+        } catch (error) {
+            console.error('Error fetching data:', error.message);
+        }
+    };
+
     return (
-        <div>Logs</div>
-    )
+        <div>
+            <h1>Logs</h1>
+            <Button onClick={fetchData}>Fetch Data</Button>
+        </div>
+    );
 }
 
 export default Logs;
