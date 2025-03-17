e1914fd289c57359ee46c5681ed5d1d2ed70883c
Nabaraj Basnet <Nabaraj Basnet>
2025-03-17 23:14:00 +0545

    path changed

--- src/app/StaffLogin/page.jsx
+++ src/app/StaffLogin/page.jsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { UserCircle, Lock, Loader2 } from 'lucide-react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import toast, { Toaster } from 'react-hot-toast';
 
 function App() {
