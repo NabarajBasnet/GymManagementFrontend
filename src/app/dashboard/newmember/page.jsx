aad479696797dbbf83b23d2d3a6302ed363f97e3
Nabaraj Basnet <nabarajbasnet2000@gmail.com>
2025-05-12 15:51:17 +0545

    UI Improved

--- src/app/dashboard/newmember/page.jsx
+++ src/app/dashboard/newmember/page.jsx
@@ -472,7 +472,7 @@ const NewMemberRegistrationForm = () => {
     const { actionTakersDB } = actionTakers || {};
 
     return (
-        <div className="w-full bg-gray-100 px-4 py-8" onClick={() => {
+        <div className="w-full bg-gray-100 px-4 py-7" onClick={() => {
             setToast(false);
             setSignUpAlert(false);
         }}>
