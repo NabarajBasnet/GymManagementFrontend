733ff69224755faf32224d3342f2932f76ad39eb
Nabaraj Basnet <nabarajbasnet2000@gmail.com>
2025-05-28 18:36:20 +0545

    Get tenant specific orders

--- src/app/clientarea/myorders/page.jsx
+++ src/app/clientarea/myorders/page.jsx
@@ -1,7 +1,52 @@
+"use client";
+
+import Loader from "@/components/Loader/Loader";
+import { toast as sonnerToast } from "sonner";
+import toast from "react-hot-toast";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+
 const MyOrders = () => {
+  const queryClient = useQueryClient();
+
+  const getMyOrders = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/api/order/tenant`, {
+        credentials: "include",
+      });
+
+      const responseBody = await response.json();
+      console.log("Response Body: ", responseBody);
+      return responseBody;
+    } catch (error) {
+      console.log("Error: ", error);
+      toast.error("Failed to fetch orders");
+      sonnerToast("Failed to fetch orders", {
+        description: "It looks like you haven't made any orders yet.",
+      });
+    }
+  };
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["myOrders"],
+    queryFn: getMyOrders,
+    retry: false,
+  });
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (error) {
+    return toast.error("Failed to fetch orders");
+  }
+
+  const { tenantOrders } = data || {};
+
+  console.log("Tenant Orders: ", tenantOrders);
+
   return (
     <div>
-      <h1>My Orders</h1>
+      <div></div>
     </div>
   );
 };
