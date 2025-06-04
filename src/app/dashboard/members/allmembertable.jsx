af13ea2b6c3fb20af6c31c06ebe439f4c70a8a94
Nabaraj Basnet <nabarajbasnet2000@gmail.com>
2025-06-05 00:14:57 +0545

    UI Issues fixed in layout

--- src/app/dashboard/members/allmembertable.jsx
+++ src/app/dashboard/members/allmembertable.jsx
@@ -2,10 +2,10 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 const Table = React.forwardRef(({ className, ...props }, ref) => (
-  <div className="relative w-full overflow-x-auto">
+  <div className="relative w-full overflow-x-auto rounded-lg border shadow-sm">
     <table
       ref={ref}
-      className={cn("min-w-max caption-bottom text-sm", className)}
+      className={cn("w-full caption-bottom text-sm", className)}
       {...props}
     />
   </div>
@@ -14,14 +14,26 @@ const Table = React.forwardRef(({ className, ...props }, ref) => (
 Table.displayName = "Table";
 
 const TableHeader = React.forwardRef(({ className, ...props }, ref) => (
-  <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...props} />
+  <thead
+    ref={ref}
+    className={cn(
+      "[&_tr]:border-b bg-gray-50 dark:bg-gray-800/50",
+      "[&_th]:whitespace-nowrap", // Prevent text wrapping in headers
+      className
+    )}
+    {...props}
+  />
 ));
 TableHeader.displayName = "TableHeader";
 
 const TableBody = React.forwardRef(({ className, ...props }, ref) => (
   <tbody
     ref={ref}
-    className={cn("[&_tr:last-child]:border-0", className)}
+    className={cn(
+      "[&_tr:last-child]:border-0 [&_tr]:border-b",
+      "[&_td]:whitespace-nowrap", // Prevent text wrapping in cells
+      className
+    )}
     {...props}
   />
 ));
@@ -31,7 +43,7 @@ const TableFooter = React.forwardRef(({ className, ...props }, ref) => (
   <tfoot
     ref={ref}
     className={cn(
-      "border-t dark:border-none bg-muted/50 font-medium [&>tr]:last:border-b-0",
+      "border-t bg-gray-50 dark:bg-gray-800/50 font-medium [&>tr]:last:border-b-0",
       className
     )}
     {...props}
@@ -43,7 +55,8 @@ const TableRow = React.forwardRef(({ className, ...props }, ref) => (
   <tr
     ref={ref}
     className={cn(
-      "border-b dark:border-none transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
+      "border-b transition-colors hover:bg-gray-100/50 dark:hover:bg-gray-800/50",
+      "data-[state=selected]:bg-gray-100 dark:data-[state=selected]:bg-gray-800",
       className
     )}
     {...props}
@@ -55,7 +68,8 @@ const TableHead = React.forwardRef(({ className, ...props }, ref) => (
   <th
     ref={ref}
     className={cn(
-      "h-10 px-1 text-center py-4 align-middle font-medium text-gray-800 [&:has([role=checkbox])]:pr-0",
+      "h-12 px-4 py-3 text-left align-middle font-medium text-gray-700 dark:text-gray-300",
+      "first:rounded-tl-lg last:rounded-tr-lg", // Rounded corners for first/last headers
       className
     )}
     {...props}
@@ -67,7 +81,7 @@ const TableCell = React.forwardRef(({ className, ...props }, ref) => (
   <td
     ref={ref}
     className={cn(
-      "py-4 text-center align-middle text-[0.9rem] [&:has([role=checkbox])]:pr-0",
+      "p-4 align-middle text-gray-800 dark:text-gray-200",
       className
     )}
     {...props}
@@ -78,7 +92,7 @@ TableCell.displayName = "TableCell";
 const TableCaption = React.forwardRef(({ className, ...props }, ref) => (
   <caption
     ref={ref}
-    className={cn("mt-4 text-sm text-muted-foreground", className)}
+    className={cn("mt-2 text-sm text-gray-500 dark:text-gray-400", className)}
     {...props}
   />
 ));
