3adc5f6ecc79a8cf732b404f959d44f91f99685b
Nabaraj Basnet <nabarajbasnet2000@gmail.com>
2025-06-19 23:18:28 +0545

    Dynamic title

--- src/app/dashboard/membertransfer/page.js
+++ src/app/dashboard/membertransfer/page.js
@@ -1,3 +1,25 @@
+import { cookies } from "next/headers";
+
+export async function generateMetadata() {
+  const cookieStore = cookies();
+  const token = cookieStore.get("loginToken")?.value;
+
+  const res = await fetch(`http://localhost:3000/api/auth/me`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    cache: "no-store",
+  });
+
+  const data = await res.json();
+
+  const orgName = data?.user?.organization?.name ?? "Dashboard";
+
+  return {
+    title: `${orgName} | Member Transfer`,
+  };
+}
+
 const MemberTransfer = () => {
   return (
     <div>
