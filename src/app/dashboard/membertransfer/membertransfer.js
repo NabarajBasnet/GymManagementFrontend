5d88a6eecdf1b8f2f9a3b760011ab3dd59e9e8b1
Nabaraj Basnet <nabarajbasnet2000@gmail.com>
2025-06-19 23:36:17 +0545

    Fetch members

--- src/app/dashboard/membertransfer/membertransfer.js
+++ src/app/dashboard/membertransfer/membertransfer.js
@@ -1,5 +1,55 @@
 "use client";
 
+import * as React from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+const frameworks = [
+  {
+    value: "next.js",
+    label: "Next.js",
+  },
+  {
+    value: "sveltekit",
+    label: "SvelteKit",
+  },
+  {
+    value: "nuxt.js",
+    label: "Nuxt.js",
+  },
+  {
+    value: "remix",
+    label: "Remix",
+  },
+  {
+    value: "astro",
+    label: "Astro",
+  },
+];
+
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { FiSearch } from "react-icons/fi";
 import {
   BiHomeAlt,
@@ -8,6 +58,7 @@ import {
 } from "react-icons/bi";
 
 // Import UI components
+import { Input } from "@/components/ui/input";
 import {
   Breadcrumb,
   BreadcrumbEllipsis,
@@ -22,9 +73,34 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { useUser } from "@/components/Providers/LoggedInUserProvider";
+import { useQuery } from "@tanstack/react-query";
 
 const MemberTransfer = () => {
   const { user, loading } = useUser();
+
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState("");
+
+  const getMembers = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/api/org-members/by-branch`
+      );
+      const resBody = await response.json();
+      return resBody;
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  };
+
+  const { data } = useQuery({
+    queryKey: ["members"],
+    queryFn: getMembers,
+  });
+
+  const { members } = data || {};
+
+  console.log("Data: ", members);
 
   return (
     <div className="w-full bg-gray-100 dark:bg-gray-900 px-4 pt-10 md:py-8">
@@ -70,7 +146,57 @@ const MemberTransfer = () => {
       </div>
 
       <div>
-        
+        <Card>
+          <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+              <Button
+                variant="outline"
+                role="combobox"
+                aria-expanded={open}
+                className="w-[200px] justify-between"
+              >
+                {value
+                  ? frameworks.find((framework) => framework.value === value)
+                      ?.label
+                  : "Select framework..."}
+                <ChevronsUpDown className="opacity-50" />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[200px] p-0">
+              <Command>
+                <CommandInput
+                  placeholder="Search framework..."
+                  className="h-9"
+                />
+                <CommandList>
+                  <CommandEmpty>No framework found.</CommandEmpty>
+                  <CommandGroup>
+                    {frameworks.map((framework) => (
+                      <CommandItem
+                        key={framework.value}
+                        value={framework.value}
+                        onSelect={(currentValue) => {
+                          setValue(currentValue === value ? "" : currentValue);
+                          setOpen(false);
+                        }}
+                      >
+                        {framework.label}
+                        <Check
+                          className={cn(
+                            "ml-auto",
+                            value === framework.value
+                              ? "opacity-100"
+                              : "opacity-0"
+                          )}
+                        />
+                      </CommandItem>
+                    ))}
+                  </CommandGroup>
+                </CommandList>
+              </Command>
+            </PopoverContent>
+          </Popover>
+        </Card>
       </div>
     </div>
   );
