5d41c31afcdf63cc821694c68b31064833d7f2b2
Nabaraj Basnet <112648953+NabarajBasnet@users.noreply.github.com>
2025-07-06 08:38:49 +0545

    Update Header.jsx

--- src/app/dashboard/Header.jsx
+++ src/app/dashboard/Header.jsx
@@ -825,7 +825,7 @@ const Header = () => {
       </div>
 
       {/* Date/Time for mobile */}
-      <div className="flex items-center md:hidden space-x-0 md:space-x-2">
+      <div className="flex items-center hidden space-x-0 md:space-x-2">
         <div className="flex items-center rounded-md px-0 py-2">
           <Calendar size={16} className="text-blue-600 mr-2" />
           <h1 className="text-sm font-medium dark:text-gray-200 text-gray-700">
